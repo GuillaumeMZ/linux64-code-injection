29a4a15ee915e90498d05fad661bb934f57425ba
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2024-06-22 00:15:27 +0200

    feat: implement the main function, which currenlty injects a library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use std::ffi::CString;
+
+use nix::unistd::Pid;
+
+mod injector;
+mod shellcodes;
+
 #[cfg(not(all(target_os = "linux", target_arch = "x86_64")))]
 compile_error!("Error: this injector only works on x86_64 Linux.");
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+
+    let target = Pid::from_raw(args[0].parse()?);
+    let dlopen_address = u64::from_str_radix(&args[1], 16)?;
+    //let dlclose_address = u64::from_str_radix(&args[2], 16)?;
+    let dl_path = CString::new(&*args[3])?;
+    let r#where = u64::from_str_radix(&args[4], 16)?;
+
+    let injection_shellcode = shellcodes::assemble_injection_shellcode(&dl_path, dlopen_address)?;
+    injector::inject_and_run_shellcode(&injection_shellcode, target, r#where)?;
+
+    Ok(())
 }
